710dc3ff9372857b7a3a8836eed6211e6e3a765a
tangpengwei <528584252@qq.com>
2023-04-11 15:34:37 +0800

    test

--- src/Test/Test.php
+++ src/Test/Test.php
@@ -1,7 +1,10 @@
 <?php
-namespace App\Test;
+namespace Tang\App\Test;
 
 class Test
 {
-
+    public function xxx():array
+    {
+        return ['今天也是充满希望的一天'];
+    }
 }
--- srcTest/Test.php
+++ srcTest/Test.php
@@ -1,14 +1,9 @@
 <?php
-namespace srcTest;
-use PHPUnit\Framework\TestCase;
-use Tang\App\Hello;
 
-class Test extends TestCase
-{
-    public function testXxx()
-    {
-        $hello = new Hello();
-        var_dump($hello->test());
-        $this->assertEquals(0,0);
-    }
-}
+require_once __DIR__ . '/../vendor/autoload.php';
+
+//$hello = new \Tang\App\Hello();
+//var_dump($hello->test());
+
+$test = new \Tang\App\Test\Test();
+var_dump($test->xxx());
